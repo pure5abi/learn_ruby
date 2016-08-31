371a033cdb2489c34243956ed6836cbe4e29202c
pure5abi <harsimran.mann.it@gmail.com>
2016-08-31 08:28:30 -0400

    update README

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,46 @@
 #write your code here
+
+def add(first_number, second_number)
+	sum = first_number + second_number
+end
+
+def subtract(first_number, second_number)
+	difference = first_number - second_number
+end  
+
+def sum(numbers)
+	total = 0
+	numbers.each{|number|
+		total += number
+	}
+
+	return total
+end
+
+
+
+def multiply(*numbers)
+	product = 1
+
+	numbers.each{|instance|
+		if instance.kind_of?(Array)
+			instance.each{|number|
+				product *= number
+			}
+		else
+			product *= instance
+		end
+	}
+	return product
+end
+
+def power(number, power)
+	result = number**power
+end
+
+def factorical(number)
+	result = number
+	number.times do 
+		result += number - 1
+	end
+end
